aa9d0fc51cae95f7933649632aa08649e8bd8a60
Haocheng <haocheng@Haochengs-MacBook-Air.local>
2026-03-14 23:04:27 +0800

    Initial commit

---
+++ src/main/java/com/example/bidirtryout/BidirtryoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.bidirtryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidirtryoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidirtryoutApplication.class, args);
+	}
+
+}
